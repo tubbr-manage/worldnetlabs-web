0cd6f311ec6268a28c494c67603c61c1ff154aa7
dhruv-tubbr <101804772+tubbr-manage@users.noreply.github.com>
2022-12-16 13:09:17 +0530

    Laptop Layout Made

--- js/main.js
+++ js/main.js
@@ -10,12 +10,22 @@
 
 // let selected;
 
+function activatetab(selected){ //selected should be element
+    tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab=>{
+        tab.classList.remove('tab-active');
+    });
+    selected.classList.add('tab-active');
+}
+
+
 $(".tab").on("click", function (event) {
     selected = event.target.getAttribute('href').slice(1);
     event.preventDefault();
-    // console.log(selected);
+    // console.log(event.target);
     document.querySelectorAll('.tab-info').forEach(tab => tab.style.display = 'none');
     document.getElementById(selected).style.display = 'block';
+    activatetab(event.target);
 });
 
 document.querySelector('.tab').click();
